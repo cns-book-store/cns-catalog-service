5575f122fcdbf3e4e762979e64210259f0c65207
Ion Pascari <ionpascari1995@gmail.com>
2023-05-11 21:48:54 +0300

    feat(skeleton): setup the base of the project

---
+++ src/main/java/inc/evil/cnscatalogservice/CnsCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package inc.evil.cnscatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inc/evil/cnscatalogservice/CnsCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.evil.cnscatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
