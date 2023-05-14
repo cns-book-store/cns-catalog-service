58e7898004f7820fc540ec1ee7548b2a0f4270d2
Ion Pascari <ionpascari1995@gmail.com>
2023-05-14 18:01:43 +0300

    feat: add validation

--- src/main/java/inc/evil/cnscatalogservice/domain/Book.java
+++ src/main/java/inc/evil/cnscatalogservice/domain/Book.java
@@ -1,4 +1,31 @@
 package inc.evil.cnscatalogservice.domain;
 
-public record Book(String isbn, String title, String author, String publisher, Integer publicationYear, Double price) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
+public record Book(
+        @NotBlank(message = "The book ISBN must be defined.")
+        @Pattern(
+                regexp = "^([0-9]{10}|[0-9]{13})$",
+                message = "The ISBN format must be valid."
+        )
+        String isbn,
+
+        @NotBlank(
+                message = "The book title must be defined."
+        )
+        String title,
+        @NotBlank(message = "The book author must be defined.")
+        String author,
+
+        String publisher,
+
+        Integer publicationYear,
+        @NotNull(message = "The book price must be defined.")
+        @Positive(
+                message = "The book price must be greater than zero."
+        )
+        Double price) {
 }
